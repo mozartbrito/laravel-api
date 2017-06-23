221d107dfb0084dd23d38c4a938ff4b96fbb61b3
Mozart Teixeira de Brito <mozart.brito@cnm.local>
2017-06-23 17:50:41 -0300

    Arquivos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //Model::unguard();
+
+        $this->call(CompaniesSeed::class);
+        $this->call(JobsSeed::class);
+
+        //Model::reguard();
     }
 }
